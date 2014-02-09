c69441de194f6e18696f19bebcc05411681e29a6
Borja <borjaminguela@gmail.com>
2014-02-09 11:29:18 +0100

    He conseguido poder modificar la plantilla en el archivo parse.py

--- parseador.py
+++ parseador.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 from xml.dom import minidom
+import xml.etree.cElementTree as ET
+
 
 def creartxt():
     archi=open('datos.txt','w')
@@ -21,6 +23,9 @@ def leertxt():
     archi.close()
 def pruebaxml():
 	arbol_dom=minidom.parse('plantilla.xml')
+	nodos=arbol_dom.childNodes
+	nodo=nodos[0].getElementsByTagName("title")[0]
+	nodo.nodeValue="adios"
 	print arbol_dom.toxml()
 
 def pruebaNodos():
@@ -30,15 +35,31 @@ def pruebaNodos():
 	print nodos[0].toxml()
 def VisualizarNodos (lista, nivel):
 	for nodo in lista:
+		nodo.text="adios"
 		print("   ")*nivel, nodo.nodeName, nodo.nodeValue
 		VisualizarNodos (nodo.childNodes, nivel + 1)
+def prueba2xml():
+	root = ET.Element("root")
 
+	doc = ET.SubElement(root, "doc")
+
+	field1 = ET.SubElement(doc, "field1")
+	field1.set("name", "blah")
+	field1.text = "some value1"
+
+	field2 = ET.SubElement(doc, "field2")
+	field2.set("name", "asdfasd")
+	field2.text = "some vlaue2"
+
+	tree = ET.ElementTree(root)
+	tree.write("filename.xml")
 
 print "hola mundo"
-creartxt()
-grabartxt()
-leertxt()
+#creartxt()
+#grabartxt()
+#leertxt()
 pruebaxml()
-pruebaNodos()
-arc_xml=minidom.parse('plantilla.xml')
-VisualizarNodos(arc_xml.childNodes,0)
+#pruebaNodos()
+#arc_xml=minidom.parse('plantilla.xml')
+#VisualizarNodos(arc_xml.childNodes,0)
+#prueba2xml()
